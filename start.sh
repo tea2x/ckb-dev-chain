ed8864114a89b85645b76ecc412f60f21a2ecf2d
ptt <70423834+tea2x@users.noreply.github.com>
2025-02-08 09:14:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-# Start ckb run in the background
-nohup ckb run > ckb_run.log 2>&1 &
+ckb run > ckb_run.log 2>&1 &
 echo $! > pid
-
-# Start ckb miner in the background
-nohup ckb miner > ckb_miner.log 2>&1 &
+ckb miner > ckb_miner.log 2>&1 &
 echo $! >> pid
 
-# Wait for background processes
-wait
+tail -f /dev/null
+exec "$@"
